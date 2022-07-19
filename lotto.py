802275b52cb9425694ea1ad92eb78665c8ac6070
code_lover <39235559+ahmadsaqr@users.noreply.github.com>
2022-07-19 13:32:03 +1200

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -1,26 +1,28 @@
 import random,re
+from termcolor import colored
+
 
 def luck(L,p="",w=""):
-    luck_st=f"\n******GOOD LUCK******\n"
+    luck_st=colored(f"\n******GOOD LUCK******\n","red")
 
     if w=="lotto":
 
         print(luck_st)
-        for index,i in enumerate(range(int(L))):
+        for i in range(int(L)):
             a=" ".join(sorted(str(i).zfill(2) for i in random.sample(range(1,41),6)))
             if p=="yes":
-                print(f"{a} \t   {str(random.randint(1,10)).zfill(2)}")
+                print(colored(f"{a} \t   {str(random.randint(1,10)).zfill(2)}","cyan"))
             else:
-                print(f"{a}")
+                print(colored(f"{a}","blue"))
         print(luck_st)
 
 
     else:
         
         print(luck_st)
-        for index,i in enumerate(range(int(L))):
+        for i in range(int(L)):
                 a=" ".join(str(i).zfill(2) for i in random.sample(range(1,41),4))
-                print(a)
+                print(colored(a,"yellow"))
         print(luck_st)
 
 
@@ -28,11 +30,11 @@ def luck(L,p="",w=""):
 def x(which=""):
     
     if which=="": 
-        which=input('do you want lotto or strike ?\n'.capitalize()).lower()
+        which=input(colored('do you want lotto or strike ?\n','red').capitalize()).lower()
 
         if  which != "lotto" and which !="strike"  :
             if re.search(r"[srike]*",which).group() :
-                ask=input("do you mean strike ? yes or no ?\n".capitalize()).lower()
+                ask=input(colored("do you mean strike ? yes or no ?\n",'red').capitalize()).lower()
                 if ask=='yes':
                     which="strike"
                 else:
@@ -40,7 +42,7 @@ def x(which=""):
 
 
             else: 
-                ask=input("do you mean lotto ? yes or no ? \n".capitalize()).lower()
+                ask=input(colored("do you mean lotto ? yes or no ? \n",'red').capitalize()).lower()
                 if ask=="yes":
                     which="lotto"
                 else:
@@ -51,24 +53,24 @@ def x(which=""):
             
             else:
                 which="strike"
-    else:            
-        
+                
+    else:             
         if which=="lotto":
             which="lotto"                
             
         else:
             which="strike"
     
-    lines=input('How many rows do you wish ?\n'.capitalize())
+    lines=input(colored('How many rows do you wish ?\n','red').capitalize())
     
     if  lines.isdigit():
         if which=="lotto":
-            power=input("Do you wish powerball ? yes or no \n".capitalize()).lower()
+            power=input(colored("Do you wish powerball ? yes or no \n",'red').capitalize()).lower()
             luck(lines,power,which)
         else:
             luck(lines)
     else:
-        print("you must only insert a number\n".capitalize())
+        print(colored("you must only insert a number\n",'red').capitalize())
         x(which)    
 
   
